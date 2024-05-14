b424a65a7b33dac1876fe2e6c5a611dba36e593c
han <rudgksdl94@gmail.com>
2024-05-14 16:07:57 +0900

    #5 - 도메인 설계: 회원 엔티티, 권한 enum,  converter

---
+++ src/main/java/com/campus/projectboardadmin/domain/converter/RoleTypesConverter.java
@@ -0,0 +1,25 @@
+package com.campus.projectboardadmin.domain.converter;
+
+import com.campus.projectboardadmin.domain.constant.RoleType;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class RoleTypesConverter implements AttributeConverter<Set<RoleType>, String> {
+
+    private static final String DELIMITER = ",";
+
+    @Override
+    public String convertToDatabaseColumn(Set<RoleType> attribute) {
+        return attribute.stream().map(RoleType::name).sorted().collect(Collectors.joining(DELIMITER));
+    }
+
+    @Override
+    public Set<RoleType> convertToEntityAttribute(String dbData) {
+        return Arrays.stream(dbData.split(DELIMITER)).map(RoleType::valueOf).collect(Collectors.toSet());
+    }
+
+}
