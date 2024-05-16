f47f2337e3238a8479fb9f484e92decad269d07f
han <rudgksdl94@gmail.com>
2024-05-16 15:45:52 +0900

    #12 - 유저 관련 dto, 컨트롤러, 서비스 스켈레톤 코드

--- src/main/java/com/campus/projectboardadmin/controller/UserAccountManagementController.java
+++ src/main/java/com/campus/projectboardadmin/controller/UserAccountManagementController.java
@@ -1,13 +1,38 @@
 package com.campus.projectboardadmin.controller;
 
+import com.campus.projectboardadmin.dto.response.UserAccountResponse;
+import com.campus.projectboardadmin.service.UserAccountManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @RequestMapping("/management/user-accounts")
 @Controller
 public class UserAccountManagementController {
 
+  private final UserAccountManagementService userAccountManagementService;
+
+  @GetMapping
+  public String userAccounts(Model model) {
+
+    return "management/user-accounts";
+  }
+
+  @ResponseBody
+  @GetMapping("/{userId}")
+  public UserAccountResponse userAccount(@PathVariable String userId) {
+    return null;
+  }
+
+  @PostMapping("/{userId}")
+  public String deleteUserAccount(@PathVariable String userId) {
+    return "redirect:/management/user-accounts";
+  }
 
 }
