364ec8cdcf083f9336a1cd8f455a3f55e374fbe6
han <rudgksdl94@gmail.com>
2024-05-16 10:14:36 +0900

    #8 - 뷰 컨트롤러, 기본 뷰

--- src/main/java/com/campus/projectboardadmin/controller/AdminAccountController.java
+++ src/main/java/com/campus/projectboardadmin/controller/AdminAccountController.java
@@ -2,9 +2,15 @@ package com.campus.projectboardadmin.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class AdminAccountController {
 
+
+  @GetMapping("/admin/members")
+  public String members() {
+    return "admin/members";
+  }
 }
